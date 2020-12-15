401d999deb33872bc073cedff1c42fff7e33e8f1
Ramzza <thezniqer@gmail.com>
2020-12-15 15:49:29 +0200

    pessimistic db

--- src/model/DataBaseSimulator.java
+++ src/model/DataBaseSimulator.java
@@ -1,24 +1,33 @@
 package model;
 
+import locks.PessimisticLockManager;
+
 public class DataBaseSimulator {
     private int counter;
     private String carName;
     private int carPrice;
+    private PessimisticLockManager lockManager;
 
     public DataBaseSimulator(int counter, String carName, int carPrice) {
         this.counter = counter;
         this.carName = carName;
         this.carPrice = carPrice;
+        this.lockManager = PessimisticLockManager.getPessimisticLockManager();
     }
 
-    public DataBaseSimulator getDraft() {
+    public DataBaseSimulator getDraft(String owner) {
+        boolean success = this.lockManager.setLock(owner, true);
+        if (!success) {
+            return null;
+        }
         return new DataBaseSimulator(this.counter, this.carName, this.carPrice);
     }
 
-    public void takeOverDraft(DataBaseSimulator db) {
+    public void takeOverDraft(String owner, DataBaseSimulator db) {
         this.counter = db.counter;
         this.carName = db.carName;
         this.carPrice = db.carPrice;
+        this.lockManager.setLock(owner, false);
     }
 
     public int getCounter() {
