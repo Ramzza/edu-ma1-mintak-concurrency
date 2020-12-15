8163b72e7fa769ba92c3c1b15dc72c1d9f3adebc
Ramzza <thezniqer@gmail.com>
2020-12-15 15:32:42 +0200

    database attr, methods

--- src/model/DataBaseSimulator.java
+++ src/model/DataBaseSimulator.java
@@ -1,17 +1,31 @@
 package model;
 
 public class DataBaseSimulator {
-    private int iCounter;
+    private int counter;
+    private String carName;
+    private int carPrice;
 
-    public DataBaseSimulator() {
-        this.iCounter = 0;
+    public DataBaseSimulator(int counter, String carName, int carPrice) {
+        this.counter = counter;
+        this.carName = carName;
+        this.carPrice = carPrice;
+    }
+
+    public DataBaseSimulator getDraft() {
+        return new DataBaseSimulator(this.counter, this.carName, this.carPrice);
+    }
+
+    public void takeOverDraft(DataBaseSimulator db) {
+        this.counter = db.counter;
+        this.carName = db.carName;
+        this.carPrice = db.carPrice;
     }
 
     public int getCounter() {
-        return this.iCounter;
+        return this.counter;
     }
 
-    public void setCounter(int iCounter) {
-        this.iCounter = iCounter;
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
