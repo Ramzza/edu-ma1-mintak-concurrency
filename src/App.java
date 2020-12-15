8b140d35ba3b8cb6ecc0ff76f4032a016d6b9920
Ramzza <thezniqer@gmail.com>
2020-12-15 02:36:02 +0200

    thread tutorial

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("hello worldcf");
+        MyThread R1 = new MyThread("Thread-1");
+        R1.start();
+
+        MyThread R2 = new MyThread("Thread-2");
+        R2.start();
     }
 }
